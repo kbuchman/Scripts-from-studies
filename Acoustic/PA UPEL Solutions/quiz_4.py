809c84e62a8171227d2b9809138479c98fff5b9c
kbuchman <kaarolbuchman@gmail.com>
2022-11-04 22:38:17 +0100

    Upgraded get_value_from_text method

--- Acoustic/PA UPEL Solutions/quiz_4.py
+++ Acoustic/PA UPEL Solutions/quiz_4.py
@@ -1,4 +1,5 @@
 # PA UPEL Quiz 4 solutions
+# using old version of get_value_from_text, needs a patch
 
 from cmath import log10
 import sys
@@ -9,23 +10,22 @@ from Scripts.get_values import get_value_from_text
 from pyautogui import write, press
 from keyboard import wait
 import math as m
-import winsound
 
 
 def task1():
-    p = get_value_from_text('równe', 'Pa,')
-    f = get_value_from_text('częstotliwości', 'Hz,')
-    t = get_value_from_text('o', 's.')
+    p = 10.6#get_value_from_text('równe', 'Pa,')
+    f = 3409.9#get_value_from_text('częstotliwości', 'Hz,')
+    t = 0.18#get_value_from_text('o', 's.')
 
     return ac.conv_ans(ac.Math.p_to_lp(p) + 3 + (10 * m.log10(1 + m.cos(2 * m.pi * f * t))) - 3) # -3 because teacher made a mistake
 
 
 def task2():
-    w1 = get_value_from_text('akustycznej', 'W')
-    w2 = get_value_from_text('i', 'W.')
-    r = get_value_from_text('odległości', 'm')
+    w1 = 12#get_value_from_text('akustycznej', 'W')
+    w2 = 4#get_value_from_text('i', 'W.')
+    r = 213.2#get_value_from_text('odległości', 'm')
 
-    print(w1, w2, r)
+    #print(w1, w2, r)
 
     lw12 = 10 * log10(pow(10, (ac.Math.w_to_lw(w1) / 10)).real + pow(10, (ac.Math.w_to_lw(w2) / 10)).real)
     return ac.conv_ans(lw12.real - (10 * log10(4 * m.pi * pow(r, 2))).real)
@@ -40,16 +40,17 @@ def test():
 
 def task3():
     #l1 = get_value_from_text('długości', 'm')  no need to use it
-    s = get_value_from_text('poprzecznym', 'm*?2', True)
-    w = get_value_from_text('mocy', 'W.')
-    l2 = get_value_from_text('odległości', 'm')
+    s = 0.4#get_value_from_text('poprzecznym', 'm*?2', True)
+    w = 3.4#get_value_from_text('mocy', 'W.')
+    l2 = 61.7#get_value_from_text('odległości', 'm')
 
     lir = ac.Math.i_to_li(w / s) + (10 * log10((4 * m.pi * pow(l2, 2))))
     return ac.conv_ans(ac.Math.lw_to_w(lir.real))
 
 def main():
-    #print(test())
-    wait('ctrl')
+    print(task1())
+    print(task3())
+    '''wait('ctrl')
     x = task1()
     write(x, interval=0)
     press('tab', presses=3, interval=0)
@@ -63,9 +64,12 @@ def main():
     z = task3()
     write(z, interval=0)
     press('tab', presses=10, interval=0)
-    press('enter', presses=2)
+
+    ac.ans(1, x)
+    ac.ans(2, y)
+    ac.ans(3, z)'''
+    
 
 if __name__ == '__main__':
     main()
-
 
--- Scripts/get_values.py
+++ Scripts/get_values.py
@@ -1,11 +1,17 @@
-from .text_from_image import get_text_from_image
-from .scrap_screenshot import take_screenshot, mouse_pos_scrap_horizontal
+from text_from_image import get_text_from_image
+from scrap_screenshot import take_screenshot, mouse_pos_scrap_horizontal
 from os import remove
+import keyboard
 
-def get_value_from_text(word_before, word_after, skip=False):
+def get_value_from_text(input_val, type=float):
+    # checking input
+    for i in input_val:
+        if len(i) != 2:
+            return None
+
     # getting screenshot of task area
     screen = take_screenshot(*mouse_pos_scrap_horizontal(100, 100, 300, 50))
-    #screen.show()
+    screen.show()
     screen.save('./screen.png')
 
     # getting text 
@@ -14,15 +20,21 @@ def get_value_from_text(word_before, word_after, skip=False):
     remove('./screen.png')
 
     # getting value 
+    values = []
     words = text.split()
-    for i, word in enumerate(words):
-        if word == word_before:
-            if words[i + 2] == word_after or skip:
-                return float(words[i + 1].replace(',', '.'))
-    return None
+    for val in input_val:
+        for i, word in enumerate(words):
+            if val[0] == word:
+                if val[1] or val[1] == word[i + 2]:
+                    values.append(type(words[i + 1].replace(',', '.')))
+                    break
+    return None if len(values) == 0 or len(values) != len(input_val) else values
 
 def main():
-    print(get_value_from_text('wynosi', 's.'))
+    search_val = (('jeszcze', True),)
+
+    keyboard.wait('ctrl')
+    print(get_value_from_text(search_val, float))
 
 if __name__ == '__main__':
     main()
